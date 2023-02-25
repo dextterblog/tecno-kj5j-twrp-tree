2404a41a04477f05615e1fe96beee1be0d3aaf50
kelechi <70432347+dextterblog@users.noreply.github.com>
2023-02-25 16:01:36 +0100

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+# Replace $$DEVICE$$ with your Device Name's Value.
+# Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
+# The part of last line in mine looks like "kg5j.mk"
+
+LOCAL_PATH := device/Tecno/kg5j
+
+PRODUCT_MAKEFILES := $(LOCAL_PATH)/kg5j.mk
